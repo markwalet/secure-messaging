2721ce3ad5aee6d5212a6a1de1ed29f846c69ee8
Mark Walet <mark.walet@gmail.com>
2021-08-07 11:29:36 +0200

    Install phpstan for analyzing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,18 +11,11 @@ use Laravel\Jetstream\Features;
 class UserFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
@@ -36,9 +29,9 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return Factory
      */
-    public function unverified()
+    public function unverified(): Factory
     {
         return $this->state(function (array $attributes) {
             return [
@@ -50,9 +43,9 @@ class UserFactory extends Factory
     /**
      * Indicate that the user should have a personal team.
      *
-     * @return $this
+     * @return Factory
      */
-    public function withPersonalTeam()
+    public function withPersonalTeam(): Factory
     {
         if (! Features::hasTeamFeatures()) {
             return $this->state([]);
